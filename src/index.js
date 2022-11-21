3dba662577d1369c5645067ba3cf42d59a422636
AlessandraSantos-dev <allems7@gmail.com>
2022-11-21 15:33:44 +0000

    Created index.html and src(index.js and style.css)

---
+++ src/index.js
@@ -0,0 +1,50 @@
+function ShowWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#weather-condition").innerHTML =
+    response.data.weather[0].main;
+}
+
+function search(city) {
+  let apiKey = "43e1e99a2b4db51809e804f5edc061cc";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&ipdi=${apiKey}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(ShowWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  search(city);
+}
+search("Lisbon");
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentDay = document.querySelector("#current-day");
+let now = new Date();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let day = now.getDay();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+currentDay.innerHTML = `${days[day]} ${hours}:${minutes}`;
