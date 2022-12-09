0a628b01dacc7717469594d7f4bc939752c92dfa
AlessandraSantos-dev <allems7@gmail.com>
2022-12-09 21:59:14 +0000

    Added forecast

--- src/index.js
+++ src/index.js
@@ -15,10 +15,12 @@ function ShowWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   celsiusTemperature = response.data.main.temp;
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
-  let apiKey = "43e1e99a2b4db51809e804f5edc061cc";
+  let apiKey = "e450bc345a80a08ada69fd5c714d871d";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&ipdi=${apiKey}&units=metric`;
   axios.get(`${apiUrl}&appid=${apiKey}`).then(ShowWeather);
 }
@@ -46,6 +48,57 @@ function showCelsiusTemperature(event) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
+function showForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#weather-forecast");
+
+  let forecastHTML = `<div class= "row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
+       
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="forecast-temperature">
+          <span class="forecast-temperature-max">${Math.round(
+            forecastDay.temp.max
+          )}ºC</span>            
+          <span class="forecast-temperature-min">${Math.round(
+            forecastDay.temp.min
+          )}ºC</span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+}
+function formatDay(timestamp) {
+  let date = new date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function getForecast(coordinates) {
+  let apiKey = "e450bc345a80a08ada69fd5c714d871d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
+}
+
 let celsiusTemperature = null;
 
 let searchForm = document.querySelector("#search-form");
@@ -58,7 +111,6 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemperature);
 
 search("Lisbon");
-
 let currentDay = document.querySelector("#current-day");
 let now = new Date();
 let hours = now.getHours();
